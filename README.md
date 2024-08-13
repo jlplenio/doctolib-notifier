0a55b7edc74f461fcf3462b6558711ddf19544d4
jlplenio <jan-lukas@plen.io>
2024-08-13 22:31:01 +0200

    filters for practitioner and for public insurance only, browser notification

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 DoctolibNotifier is a Python package that automates checking for available appointment slots on the German Doctolib and notifies the user when slots are found. It leverages the [Nodriver package](https://github.com/ultrafunkamsterdam/nodriver/) for browser automation and data retrieval.
 
 ## How it works
-The `DoctolibNotifier` script automates checking for available appointment slots on Doctolib. It takes a Doctolib URL, parses it to extract query parameters, and builds a query string to fetch availability data. The script checks for available slots every 60 seconds. If slots within 15 days of today are found, it sounds a notification and prints the available dates to the console. If there are slots available beyond 15 days, it provides information on the next available slot.
+The `DoctolibNotifier` script automates checking for available appointment slots on Doctolib - __for public insurance only__. It takes a Doctolib URL, parses it to extract query parameters, and builds a query string to fetch availability data. The script checks for available slots every 60 seconds. If slots within 15 days of today are found, it sounds a notification and prints the available dates to the console. If there are slots available beyond 15 days, it provides information on the next available slot.
 
 
 ## Usage
 
-The authors recommend using Python 3.11. Follow these steps:
+Chrome browser needs to be installed on the system. The authors recommend using Python 3.11. Follow these steps:
 
 1. **Install Poetry**: If you don't have Poetry installed, you can install it by following the [Poetry installation guide](https://python-poetry.org/docs/#installation).
 2. **Navigate to the Project's Root Directory**: Open a terminal and navigate to the root directory of the project.
@@ -23,5 +23,5 @@ The authors recommend using Python 3.11. Follow these steps:
 5. **Grab the Doctolib URL**: Navigate to the final appointments page and copy the url:  
 ![grafik](https://github.com/user-attachments/assets/2f47a0a8-1d56-4e84-82f6-0b3fdd6a7a56)
 
-
 6. Insert the url when prompted in the console.
+7. If appointments within 15 days are found, your browser will open with the url.
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from datetime import datetime
 
 from urllib.parse import urlparse, parse_qs, unquote
 
-import winsound
+import webbrowser
 
 from loaders import NoDriver
 
 
 class DoctolibNotifier:
-
     def __init__(self):
         self.driver = NoDriver()
 
@@ -22,39 +21,67 @@ class DoctolibNotifier:
         practice_name = unquote(parsed_url.path.split('/')[3])
         practice_id = query_params.get('placeId', [""])[0].split("-")[1]
         motive_ids = query_params.get('motiveIds[]', [])
+        practitioner_id = query_params.get('practitionerId', [])
+        practitioner_id = [p_id for p_id in practitioner_id if p_id != 'NO_PREFERENCE']
 
-        return practice_name, practice_id, motive_ids
+        return practice_name, practice_id, motive_ids, practitioner_id
 
     def fetch_practice_data(self, practice_name):
         url = f"https://www.doctolib.de/online_booking/draft/new.json?id={practice_name}"
         response_json = self.query_doctolib_to_json(url)
         return response_json
 
-    def extract_agenda_ids(self, data, motive_ids):
+    def extract_agenda_ids(self, data, motive_ids, practitioner_id):
         """Extract agenda IDs from practice data that match the specified motive IDs."""
         agenda_ids = []
-        if 'data' in data and 'agendas' in data['data']:
+        if 'data' in data and 'agendas' in data.get('data'):
             for agenda in data['data']['agendas']:
                 # Check if 'visit_motive_ids' exists and has any matching motive_id
-                if 'visit_motive_ids' in agenda and any(
-                        int(motive_id) in agenda['visit_motive_ids'] for motive_id in motive_ids):
-                    # Safely get the agenda 'id'
-                    agenda_id = agenda.get('id')
-                    if agenda_id is not None:
-                        agenda_ids.append(str(agenda_id))
+                if 'visit_motive_ids' in agenda:
+                    if any(int(motive_id) in agenda.get('visit_motive_ids') for motive_id in motive_ids):
+                        if not practitioner_id or str(agenda.get('practitioner_id')) in practitioner_id:
+                            agenda_id = agenda.get('id')
+                            if agenda_id is not None:
+                                agenda_ids.append(str(agenda_id))
         return agenda_ids
 
-    def construct_final_url(self, motive_ids, agenda_ids, practice_id, insurance_sector, telehealth, start_date, limit):
+    def filter_agenda_ids(self, data, motive_ids, raw_agenda_ids):
+        global filtered_agenda_ids
+        if 'data' in data and 'visit_motives' in data.get('data'):
+            configurations = [
+                motive.get('configurations')
+                for motive in data.get('data').get('visit_motives')
+                if str(motive.get('id')) in motive_ids
+            ]
+            configuration = configurations[0]  # ONLY filters for first motive, never seen multiple motives
+
+            filtered_agenda_ids = [
+                agenda_id
+                for agenda_id in raw_agenda_ids
+                if not any(
+                    str(config["agenda_id"]) == agenda_id
+                    and config["insurance"] == "public"
+                    and config["disabled"] is True
+                    for config in configuration
+                )
+            ]
+
+        return filtered_agenda_ids
+
+    def construct_final_url(
+        self, motive_ids, agenda_ids, practice_id, insurance_sector, telehealth, start_date, limit
+    ):
         """Construct the final URL to fetch availabilities."""
         motive_ids = ','.join(motive_ids)
         agenda_ids = '-'.join(agenda_ids)
-        url = (f"https://www.doctolib.de/availabilities.json?visit_motive_ids={motive_ids}&agenda_ids={agenda_ids}"
-               f"&practice_ids={practice_id}&insurance_sector={insurance_sector}&telehealth={telehealth}"
-               f"&start_date={start_date}&limit={limit}")
+        url = (
+            f"https://www.doctolib.de/availabilities.json?visit_motive_ids={motive_ids}&agenda_ids={agenda_ids}"
+            f"&practice_ids={practice_id}&insurance_sector={insurance_sector}&telehealth={telehealth}"
+            f"&start_date={start_date}&limit={limit}"
+        )
         return url
 
     def query_doctolib_to_json(self, url):
-
         response = self.driver.get_response(url)
 
         match = re.search(r'<pre>(.*?)</pre>', response, re.DOTALL)
@@ -63,17 +90,17 @@ class DoctolibNotifier:
         response_json = json.loads(json_string)
         return response_json
 
-    def search_loop(self, final_url):
+    def search_loop(self, final_url, initial_url):
         while True:
             available_dates = []
             response_json = self.query_doctolib_to_json(final_url)
 
             for availability in response_json['availabilities']:
-                if availability["slots"]:
+                if availability.get("slots"):
                     available_dates.append(availability["date"])
 
             if available_dates:
-                winsound.Beep(500, 800)
+                webbrowser.open(initial_url)
                 print(f"The following dates are available: {available_dates}")
 
             next_slot = response_json.get('next_slot')
@@ -85,13 +112,16 @@ class DoctolibNotifier:
 
     def main(self, initial_url):
         # Construct final query url
-        practice_name, practice_id, motive_ids = self.parse_initial_url(initial_url)
+        practice_name, practice_id, motive_ids, practitioner_id = self.parse_initial_url(initial_url)
         practice_data = self.fetch_practice_data(practice_name)
-        agenda_ids = self.extract_agenda_ids(practice_data, motive_ids)
+        raw_agenda_ids = self.extract_agenda_ids(practice_data, motive_ids, practitioner_id)
+        agenda_ids = self.filter_agenda_ids(practice_data, motive_ids, raw_agenda_ids)
+        final_url = self.construct_final_url(
+            motive_ids, agenda_ids, practice_id, 'public', 'false', datetime.today().strftime('%Y-%m-%d'), '15'
+        )
 
-        final_url = self.construct_final_url(motive_ids, agenda_ids, practice_id, 'public', 'false',
-                                             datetime.today().strftime('%Y-%m-%d'), '15')
-        self.search_loop(final_url)
+        print(f"The final query url is {final_url}")
+        self.search_loop(final_url, initial_url)
 
 
 if __name__ == "__main__":
